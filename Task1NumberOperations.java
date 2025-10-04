2b7d56c398dfedb7e1f89f4c2bd54b59f3d453c0
Alex_Keks <125218496+G0o0DM4N@users.noreply.github.com>
2025-10-04 08:03:03 +0300

    Update Task1NumberOperations.java

--- Task1NumberOperations.java
+++ Task1NumberOperations.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class NumberOperations {
+public class Task1NumberOperations {
 
     public static void main(String[] args) {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
